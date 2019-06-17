6c7f5dba581d2cd0b6ab175fd6d92c4b7fb348e3
JSKwok <justin.s.kwok@gmail.com>
2019-06-17 10:24:41 -0400

    Added functionality for writing a review. Reversed display order of reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,16 +3,14 @@ class ReviewsController < ApplicationController
   def create
 
     @product = Product.find(params[:product_id])
-    puts current_user
     @review = Review.new(review_params)
-    puts 'Here we are', @review.attributes
+    @review.user_id = current_user.id
+    @review.product_id = @product.id
     @review.save
-    puts 'After the save'
     if @review.save
       redirect_to product_path(@product)
     else
       redirect_to product_path(@product)
-      puts "Shit failed you asshole."
     end
 
   end
@@ -22,12 +20,5 @@ class ReviewsController < ApplicationController
   def review_params
     params.require(:review).permit(:rating, :description)
   end
-
-
-# find the parent model
-# Use the has_many association to initialize the child instance
-# Attempt to save
-# If successful: redirect to appropriate page
-# Else: render the page where the form resides
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -5,6 +5,6 @@ class Review < ActiveRecord::Base
   validates :product_id, presence: true
   validates :user_id, presence: true
   validates :description, presence: true
-  validates :rating, presence: true
+  validates :rating, presence: true, numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 1 }
 
 end
